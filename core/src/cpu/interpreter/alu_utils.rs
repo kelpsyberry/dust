56d76b9b49693aef29bdd200b2fb96f2741ca2da
kelpsyberry <138107494+kelpsyberry@users.noreply.github.com>
2022-01-05 13:36:53 +0100

    Why did I have this bad idea and why did I stick with it?

--- core/src/cpu/interpreter/alu_utils.rs
+++ core/src/cpu/interpreter/alu_utils.rs
@@ -4,6 +4,7 @@ use cfg_if::cfg_if;
 cfg_if! {
     // TODO: The specialized x86 handlers have very questionable usefulness, maybe they should be
     // scrapped.
+    // Obscure bugs this has caused so far: 1
     if #[cfg(any(target_arch = "x86_64", target_arch = "x86"))] {
         mod x86;
         pub use x86::*;
