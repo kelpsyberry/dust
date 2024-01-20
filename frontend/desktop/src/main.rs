b029fe4562e5a89e48be0f9a8afff2927ebf423d
kelpsyberry <138107494+kelpsyberry@users.noreply.github.com>
2024-01-20 02:54:02 +0100

    Fix macOS app bundles again

--- frontend/desktop/src/main.rs
+++ frontend/desktop/src/main.rs
@@ -39,32 +39,6 @@ mod emu;
 mod ui;
 
 fn main() {
-    #[cfg(all(target_os = "macos", app_bundle))]
-    {
-        use cocoa::{
-            base::{id, nil},
-            foundation::{NSBundle, NSString},
-        };
-        use std::{env::set_current_dir, ffi::CStr};
-        let path = (|| unsafe {
-            let main_bundle = id::mainBundle();
-            if main_bundle == nil {
-                return None;
-            }
-            let resource_path = main_bundle.resourcePath();
-            if resource_path == nil {
-                return None;
-            }
-            let result = CStr::from_ptr(resource_path.UTF8String())
-                .to_str()
-                .ok()
-                .map(str::to_string);
-            let _: () = msg_send![resource_path, release];
-            result
-        })()
-        .expect("Couldn't get bundle resource path");
-        set_current_dir(path).expect("Couldn't change working directory to bundle resource path");
-    }
-
+    emu_utils::setup_current_dir();
     ui::main();
 }
