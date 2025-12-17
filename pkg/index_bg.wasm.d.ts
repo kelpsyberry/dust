d86878c2415d71a33d1b1414fb5d5749970d0bfd
kelpsyberry <kelpsyberry@users.noreply.github.com>
2025-12-17 21:55:51 +0000

    deploy: 76d79f25e4ea3ce355c4ac868f5ac9a233e64b89

---
+++ pkg/index_bg.wasm.d.ts
@@ -0,0 +1,22 @@
+/* tslint:disable */
+/* eslint-disable */
+export const __wbg_emustate_free: (a: number, b: number) => void;
+export const create_emu_state: (a: number, b: number, c: number, d: any, e: number, f: number, g: number, h: number, i: any) => number;
+export const emustate_export_save: (a: number) => any;
+export const emustate_load_save: (a: number, b: any) => void;
+export const emustate_reset: (a: number) => void;
+export const emustate_run_frame: (a: number) => any;
+export const emustate_update_input: (a: number, b: number, c: number) => void;
+export const emustate_update_touch: (a: number, b: number, c: number) => void;
+export const internal_get_memory: () => any;
+export const internal_get_module: () => any;
+export const run_worker: () => void;
+export const memory: WebAssembly.Memory;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_thread_destroy: (a?: number, b?: number, c?: number) => void;
+export const __wbindgen_start: (a: number) => void;
